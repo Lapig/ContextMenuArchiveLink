cc6c7bdf25723bbec3514cd83c27e48191f19719
lapig <pig@lapig.xyz>
2017-11-18 04:03:54 -0500

    cleanup comments/old code

--- script.js
+++ script.js
@@ -5,14 +5,9 @@
 // Archive context callback
 function genericOnClick(info, tab) {
   window.open("http://archive.is/?url="+info.linkUrl,"self")
-  //console.log("info: " + JSON.stringify(info));
-  //console.log("tab: " + JSON.stringify(tab));
-  /*chrome.tabs.create({  
-    url: "http://www.google.com/search?q=" + info.selectionText,
-  }); */
 }
 
-// Create one test item for each context type.
+// Create context menu entry
 var title = "Archive link";
 chrome.contextMenus.create({
   "title": title, "contexts":["link"],
